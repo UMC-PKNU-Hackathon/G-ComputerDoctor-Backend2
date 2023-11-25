7f0eb430dba96fe301194710a3b05871e27fd396
taeyeongKims <137798168+taeyeongKims@users.noreply.github.com>
2023-11-25 10:59:24 +0900

    Delete src directory

--- src/models/user.sql.js
+++
@@ -1,9 +0,0 @@
-// models/user.sql.js
-
-export const insertUserSql = "INSERT INTO users (nickname, personal_id, password) VALUES (?, ?, ?);";
-
-export const getUserID = "SELECT * FROM users WHERE id = ?";
-
-export const confirmId = "SELECT EXISTS(SELECT 1 FROM users WHERE personal_id = ?) as isExistId";
-
-export const getUserPassword = "SELECT password FROM users WHERE personal_id = ?";
